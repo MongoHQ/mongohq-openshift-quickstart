86c258dd84caa629519fa316f8b79aba350f9922
Chris Winslett <chris@mongohq.com>
2013-11-19 22:34:34 -0800

    Nicer, cleaner Sinatra code

--- quick_start.rb
+++ quick_start.rb
@@ -10,10 +10,16 @@ class QuickStart < Sinatra::Base
   enable :sessions
   set :public_folder, Proc.new { File.join(root, "static") }
 
+  CONNECTION_PROCESS = [
+    :connection_string_present,
+    :connection_string_valid,
+    :able_to_connect,
+    :able_to_authenticate,
+    :able_to_write
+  ]
+
   before do
-    if ENV['MONGOHQ_URL'] =~ /^mongodb:\/\//
-      redirect "/test_connection_string"
-    end
+    @connection_state = get_connection_state
   end
 
   get "/" do
@@ -37,102 +43,64 @@ class QuickStart < Sinatra::Base
   end
 
   get "/add-environmental-variable" do
-    @configure_environmental_variable = <<-CODE
-$ gem install rhc
-$ rhc setup
-$ rhc set-env MONGO_URL=<MONGO URI> -a <app name>
-    CODE
-
     haml :add_environmental_variable
   end
 
   get "/test-mongohq-connection" do
-    begin
-      @checks = []
-      @success = false
+    haml :test_mongohq_connection
+  end
 
-      if ENV['MONGO_URL'] && ENV['MONGO_URL'] != ''
-        @checks << {status: "correct", message: "An environmental variable is set for <span class='code'>MONGO_URL</span>."}
-
-        @db = Moped::Session.connect(ENV['MONGO_URL'])
-        @checks << {status: "correct", message: "A correctly formatted <span class='code'>MONGO_URL</span> is provided."}
-
-        @db.command({isMaster: 1})
-        @checks << {status: "correct", message: "We've connected to the MongoDB running at your given <class='code'>MONGO_URL</span>."}
-
-        @db["system.namespaces"].find().each { |i| i } # hack to actually execute the query
-
-        @checks << {status: "correct", message: "We've authenticated and queried against the database."}
-
-        @db["congratulations"].insert({name: "Congratulations from MongoHQ! You've connected, and are on your way to using your database."})
-
-        @checks << {status: "correct", message: "A document has been added to the congratulations collection for your database.  View it from the MongoHQ console."}
-
-        @success = true
+  def get_connection_state
+    state_array = []
+    CONNECTION_PROCESS.map do |step|
+      if send("test_#{step}".to_sym, ENV['MONGO_URL'])
+        state_array << step
       else
-        @checks << {status: "error", message: "No environmental variable is set for <span class='code'>MONGO_URL</span>", link: "/add-environmental-variable"}
+        break
       end
-
-      haml :test_mongohq_connection
-    rescue Moped::Errors::InvalidMongoURI
-      @checks << {status: "error", message: <<-MESSAGE, link: "/add-environmental-variable"}
-Improperly formatted <span class='code'>MONGO_URL</span>.  Your connection string is:</p>
-
-<span class='code'>#{ENV['MONGO_URL']}</span>
-
-<p>A proper connections string looks like:</p>
-
-<span class='code'>mongodb://username:password@host:port/database-name</span>
-      MESSAGE
-
-      haml :test_mongohq_connection
-    rescue Moped::Errors::ConnectionFailure
-      @checks << {status: "error", message: <<-MESSAGE, link: "/add-environmental-variable"}
-No Mongo accessible at <span class='code'>MONGO_URL</span>.  Your connection string is:</p>
-
-<span class='code'>#{ENV['MONGO_URL']}</span>
-
-<p>To resolve, double check the <span class='code'>host:port</span> portion of the connection string that is formatted like so:</p>
-
-<span class='code'>mongodb://username:password@host:port/database-name</span>
-
-<p>This error means the application cannot connect to a Mongo running at the given <span class='code'>host:port</span>.</p>
-      MESSAGE
-
-      haml :test_mongohq_connection
-    rescue Moped::Errors::QueryFailure
-      error = $1.to_i if $!.message =~ /error ([0-9]+):/
-
-      case error
-      when 16550
-      @checks << {status: "error", message: <<-MESSAGE, link: "/add-user-to-mongohq-database"}
-The following error was returned when querying your database:
-<pre>
-#{$!.message}
-</pre>
-
-Typically, this is a username / password error.
-      MESSAGE
-
-      haml :test_mongohq_connection
-
-      else
-        raise error.inspect
-      end
-    rescue Moped::Errors::AuthenticationFailure
-      @checks << {status: "error", message: <<-MESSAGE, link: "/add-user-to-mongohq-database"}
-Authentication failed for given <span class='code'>MONGO_URL</span>.  Your connection string is:</p>
-
-<span class='code'>#{ENV['MONGO_URL']}</span>
-
-<p>To resolve, double check the <span class='code'>username:password</span> portion of the connection string that is formatted like so:</p>
-
-<span class='code'>mongodb://username:password@host:port/database-name</span>
-
-<p>This error message means the connection succeeded, but the authentication failed.</p>
-      MESSAGE
-
-      haml :test_mongohq_connection
     end
+
+    @connection_success = state_array == CONNECTION_PROCESS
+
+    state_array
+  end
+
+  #
+  # Series of tests for the presence and validity of the MONGO_URL
+  #
+  def test_connection_string_present(uri)
+    uri && uri != ''
+  end
+
+  def test_connection_string_valid(uri)
+    uri =~ Moped::MongoUri::URI
+  end
+
+  def test_able_to_connect(uri)
+    Timeout::timeout(5) do
+      uri_without_auth = uri.gsub(/\/[^\:\\]+:[^\:\@]+\@/, '//')
+      s = Moped::Session.connect(uri_without_auth + "?timeout=5")
+      s.command({isMaster: 1})
+    end
+  rescue
+    false
+  end
+
+  def test_able_to_authenticate(uri)
+    Timeout::timeout(5) do
+      s = Moped::Session.connect(uri + "?timeout=5")
+      s["system.namespaces"].find().count()
+    end
+  rescue
+    false
+  end
+
+  def test_able_to_write(uri)
+    Timeout::timeout(5) do
+      s = Moped::Session.connect(uri + "?safe=true&timeout=5")
+      s["congratulations"].insert({name: "Congratulations from MongoHQ! You've connected, and are on your way to using your MongoHQ database."})
+    end
+  rescue
+    false
   end
 end
